e459d43fdb7ecb62744eb0e5ccedffa7c514da6f
Sharon Yang <sharonyyang0@gmail.com>
2024-04-26 16:47:58 -0400

    #61 changed last icon import

--- src/pages/experiences.tsx
+++ src/pages/experiences.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Button } from '@mui/material';
-import DownloadIcon from '@mui/icons-material/Download';
+import { Download } from '@mui/icons-material';
 import PageHeading from '../components/PageHeading';
 import ResumeExperiencesSection from '../components/ResumePage/ResumeExperiencesSection';
 import { RESUME_GOOGLE_DRIVE_ID } from '../data/Pages/ResumePageData';
@@ -15,7 +15,7 @@ const ExperiencePage = () => {
             href={`https://drive.google.com/uc?export=download&id=${RESUME_GOOGLE_DRIVE_ID}`}
             variant='contained'
             size='large'
-            endIcon={<DownloadIcon />}
+            endIcon={<Download />}
           >
             resume
           </Button>
