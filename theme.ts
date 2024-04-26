080cebd321c7a8ef64087f57b931aa298d04b23a
Sharon Yang <sharonyyang0@gmail.com>
2024-04-26 19:16:32 -0400

    #68 updating theme

--- theme.ts
+++ theme.ts
@@ -1,4 +1,4 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 
 const theme = createTheme({
   palette: {
@@ -66,4 +66,4 @@ const theme = createTheme({
   },
 });
 
-export default theme;
+export default responsiveFontSizes(theme);
