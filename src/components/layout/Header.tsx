7ea223c39d0f436ae8d1c08c301a5844f5b0fd95
Sharon Yang <sharonyyang0@gmail.com>
2025-09-28 23:20:19 -0400

    add logo

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,12 @@
 import Typography from "@/components/base/Typography";
 import headerLinks from "@/content/headerLinks.json";
+import Logo from "@/assets/Logo";
 
 const Header = () => {
   return (
     <header className="w-full">
-      <div className="py-8 flex justify-between">
-        <Typography>Sharon Yang</Typography>
+      <div className="py-8 flex justify-between items-center">
+        <Logo className="w-16"/>
         <nav className="flex gap-8">
           {headerLinks.map((link: string) => (
             <a key={link} href={`#${link}`}>
