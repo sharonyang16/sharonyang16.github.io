886f7e9e888af33d2ce12cf279fc30cde3f95664
Sharon Yang <sharonyyang0@gmail.com>
2025-09-28 23:32:55 -0400

    responsive css changes

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,15 +5,17 @@ import Logo from "@/assets/Logo";
 const Header = () => {
   return (
     <header className="w-full">
-      <div className="py-8 flex justify-between items-center">
-        <Logo className="w-16"/>
-        <nav className="flex gap-8">
-          {headerLinks.map((link: string) => (
-            <a key={link} href={`#${link}`}>
-              <Typography intent={"label1"}>{link}</Typography>
-            </a>
-          ))}
-        </nav>
+      <div className="py-8 flex lg:justify-between justify-center items-center">
+        <Logo className="w-16" />
+        <div className="hidden lg:block">
+          <nav className="flex gap-8">
+            {headerLinks.map((link: string) => (
+              <a key={link} href={`#${link}`}>
+                <Typography intent={"label1"}>{link}</Typography>
+              </a>
+            ))}
+          </nav>
+        </div>
       </div>
       <hr />
     </header>
