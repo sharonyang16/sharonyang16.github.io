c50561d5977614b02057a10b924d345538ac88d9
Sharon Yang <92332953+sharonyang16@users.noreply.github.com>
2025-10-30 19:12:38 -0400

    Add more thumbnails (#40)
    * add 5 project thumbnails
    
    * edit descriptions
    
    * add placeholder + chewy thumbnail + reorganize projects

--- src/components/home/Projects/ProjectCard.tsx
+++ src/components/home/Projects/ProjectCard.tsx
@@ -11,38 +11,36 @@ const ProjectCard = ({
   technologies,
 }: ProjectCardProps) => {
   return (
-    <a
-      href={url}
-      target="_blank"
-      rel="noopener noreferrer"
-      className={`border-1 border-black dark:border-white rounded-lg ${
-        url ? "hover:shadow-xl dark:hover:bg-gray-950" : ""
-      }`}
-    >
-      <div className="flex flex-col p-4 gap-2 h-full">
-        <div className="flex justify-center items-center w-full h-fit flex-grow-1 relative">
-          {thumbnail && (
-            <Image
-              src={thumbnail}
-              alt={`${title} thumbnail`}
-              width="1600"
-              height="900"
-              className="w-full h-auto"
-            />
-          )}
-        </div>
-
-        <Typography intent="subheadding2">{title}</Typography>
-        <Typography intent="paragraph2">{description}</Typography>
-        <div className="flex flex-wrap gap-2">
-          {technologies.map((name) => (
-            <Chip key={name}>
-              <Typography intent="label2">{name}</Typography>
-            </Chip>
-          ))}
-        </div>
+    <div className="flex flex-col gap-2 h-full">
+      <div className="flex justify-center items-center w-full h-fit relative">
+        <Image
+          src={thumbnail ?? "/assets/placeholder.png"}
+          alt={`${title} thumbnail`}
+          width="1600"
+          height="900"
+          className="w-full h-auto"
+        />
       </div>
-    </a>
+      <a
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={` ${
+          url ? "hover:text-gray-700 dark:hover:text-gray-300" : ""
+        }`}
+      >
+        <Typography intent="subheadding2">{title}</Typography>
+      </a>
+
+      <Typography intent="paragraph2">{description}</Typography>
+      <div className="flex flex-wrap gap-2">
+        {technologies.map((name) => (
+          <Chip key={name}>
+            <Typography intent="label2">{name}</Typography>
+          </Chip>
+        ))}
+      </div>
+    </div>
   );
 };
 
