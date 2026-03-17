f8092dae3a2008f9df6406410c1f14f111928f40
Sharon Yang <sharonyyang0@gmail.com>
2026-03-16 21:50:53 -0400

    experience section styling

--- src/components/home/Experiences/ExperienceCard.tsx
+++ src/components/home/Experiences/ExperienceCard.tsx
@@ -13,14 +13,17 @@ const ExperienceCard = ({
 }: ExperienceCardProps) => {
   return (
     <div className="flex flex-col gap-2">
-      <Typography intent="subheadding2">{`${position} @ ${company}`}</Typography>
-      <Typography
-        intent="label2"
-        className="text-gray-600 dark:text-gray-400"
-      >{`${format(start, "MMM yyyy").toUpperCase()} - ${format(
-        end,
-        "MMM yyyy",
-      ).toUpperCase()}`}</Typography>
+      <div className="flex flex-col gap-1">
+        <Typography intent="subheadding2">{`${position} @ ${company}`}</Typography>
+        <Typography
+          intent="label2"
+          className="text-gray-600 dark:text-gray-400"
+        >{`${format(start, "MMM yyyy").toUpperCase()} - ${format(
+          end,
+          "MMM yyyy",
+        ).toUpperCase()}`}</Typography>
+      </div>
+
       <Typography intent="paragraph2">{description}</Typography>
       <div className="flex flex-wrap gap-2">
         {technologies.map((name) => (
