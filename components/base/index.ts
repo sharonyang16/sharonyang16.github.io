78e9fe1cd638d75396abbab3afb4fd064263b3ec
Sharon Yang <sharonyyang0@gmail.com>
2026-05-05 18:26:34 -0400

    create barrel file

---
+++ components/base/index.ts
@@ -0,0 +1,4 @@
+export { default as Button } from "./Button/Button";
+export { default as Chip } from "./Chip/Chip";
+export { default as Link } from "./Link/Link";
+export { default as Typography } from "./Typography/Typography";
